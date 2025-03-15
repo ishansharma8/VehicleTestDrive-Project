b884be11f26f95041b7a0c415d8c507e5610625c
Ishan <ishansharma11228@gmail.com>
2025-03-15 11:09:41 +0530

    Implementing DI for vehicle service class./IOC in vehicle service(Controllers or other services depend on this interface rather than a concrete class)

--- VehicleApi/Program.cs
+++ VehicleApi/Program.cs
@@ -1,3 +1,6 @@
+using VehicleApi.Interface;
+using VehicleApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IVehicle, VehicleService>();
+//Responsibility of AddScoped which will get a new instance of vehicleservice class
 
 var app = builder.Build();
 
