a7faccf6b78773e0d033065951b81141b4ac05f9
Ishan <ishansharma11228@gmail.com>
2025-03-16 01:57:44 +0530

    Icustomer Interface and CustomerService

---
+++ CustomersApi/Services/CustomerService.cs
@@ -0,0 +1,28 @@
+﻿using CustomersApi.Data;
+using CustomersApi.Interfaces;
+using CustomersApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomersApi.Services
+{
+    public class CustomerService:ICustomer
+    {
+        private ApiDbContext _dbcontext;
+        public CustomerService()
+        {
+            _dbcontext = new ApiDbContext();
+        }
+        public async Task AddCustomer(Customer customer) {
+            var vehiclInDb =await _dbcontext.Vehicles.FirstOrDefaultAsync(v => v.Id == customer.VehicleId);
+            if(vehiclInDb == null)
+            {
+                await _dbcontext.Vehicles.AddAsync(customer.Vehicle);
+                await _dbcontext.SaveChangesAsync();
+            }
+            customer.Vehicle = null;
+            await _dbcontext.AddAsync(customer);
+            await _dbcontext.SaveChangesAsync();
+
+        }
+    }
+}
