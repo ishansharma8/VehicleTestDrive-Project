05d22b313cf18b79901c86e42766bf0f6dd6694e
Ishan <ishansharma11228@gmail.com>
2025-03-15 10:57:48 +0530

    Interface for vehicle Service

---
+++ VehicleApi/Interface/IVehicle.cs
@@ -0,0 +1,14 @@
+﻿using VehicleApi.Models;
+
+namespace VehicleApi.Interface
+{
+    public interface IVehicle
+    {
+        //get all ,get one by id ,post 1 by id ,update one by id .,delelte 1 by id
+        Task<List<Vehicle>> GetAllVehicles();  //returns list of vehicles
+        Task<Vehicle> GetVehicleById(int id);
+        Task AddVehicle(Vehicle vehicle);
+        Task UpdateVehicle(int id, Vehicle vehicle);
+        Task DeleteVehicle(int id);  //if not return anything then task
+    }
+}
