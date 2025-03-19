43e5f19ff87a8e9f6fc5a2f6ae4dd03da237e373
Ishan <ishansharma11228@gmail.com>
2025-03-19 12:59:31 +0530

    Azure sender service -But only string will be send

--- CustomersApi/Services/CustomerService.cs
+++ CustomersApi/Services/CustomerService.cs
@@ -1,4 +1,5 @@
-﻿using CustomersApi.Data;
+﻿using Azure.Messaging.ServiceBus;
+using CustomersApi.Data;
 using CustomersApi.Interfaces;
 using CustomersApi.Models;
 using Microsoft.EntityFrameworkCore;
@@ -22,6 +23,23 @@ namespace CustomersApi.Services
             customer.Vehicle = null;
             await _dbcontext.AddAsync(customer);
             await _dbcontext.SaveChangesAsync();
+
+            //Adding code for Azure Messaging Bus
+            //For conn string :azure portal->service bus->shared access policies->root manage shared access key->Primary connection string
+            string connectionString = "Endpoint=...";
+            string queueName = "azure queue";
+
+            await using ServiceBusClient client = new(connectionString); // since ServiceBusClient implements IAsyncDisposable we create it with "await using"
+
+            //This code is perfect to use but it will only send a string to the azure service bus. But we want to send the customer object. so we will first serialise the 
+            //customer object to json and then we will send the customer object(Pushing uptill here)
+
+            ServiceBusSender sender = client.CreateSender(queueName);// create the sender
+
+            ServiceBusMessage message = new("Hello world!"); // create a message that we can send. UTF-8 encoding is used when providing a string.
+
+            // send the message
+            await sender.SendMessageAsync(message);
 
         }
     }
