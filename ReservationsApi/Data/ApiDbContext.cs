91c63e4759826582124017b380847eaee42798dc
Ishan <ishansharma11228@gmail.com>
2025-04-01 01:14:20 +0530

    Adding DbContext class

---
+++ ReservationsApi/Data/ApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReservationsApi.Models;
+
+namespace ReservationsApi.Data
+{
+    public class ApiDbContext:DbContext
+    {
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-O7AKC939\SQLEXPRESS;Database=ReservationApiDb;Trusted_Connection=true;");
+        }
+    }
+}
